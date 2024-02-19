27220b4cb384d4c42c65529998bf997fa329a69c
Pyrske <kalib.laclair@mymail.champlain.edu>
2024-02-18 21:20:43 -0500

    Added a damage noise, designed menus, modified some sprites, and replaced spikes with animated flames
    The game is "complete", although with more time some better features could have been added.

--- Assets/Scripts/PlayerRespawn.cs
+++ Assets/Scripts/PlayerRespawn.cs
@@ -5,10 +5,18 @@ using UnityEngine;
 public class PlayerRespawn : MonoBehaviour
 {
     [SerializeField] private Transform respawn;
+    private AudioSource _damageNoise;
+
+    private void Start()
+    {
+        _damageNoise = GetComponent<AudioSource>();
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Hazard"))
         {
+            _damageNoise.PlayOneShot(_damageNoise.clip);
             transform.position = respawn.transform.position;
         }
         else if (other.gameObject.CompareTag("Checkpoint"))
